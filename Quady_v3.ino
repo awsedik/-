ee004e55aca7b0ae4798edfbf0a09f08d6294838
awsedik <artemiirolin328@gmail.com>
2025-04-02 21:31:16 +0300

    Update Quady_v3.ino

--- Quady_v3.ino
+++ Quady_v3.ino
@@ -1,41 +1,38 @@
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
-LiquidCrystal_I2C LCD(0x38, 16, 2); // Инициализация LCD дисплея с адресом 0x38 и размером 16x2
-const int a = 4; // Размерность массивов
+LiquidCrystal_I2C LCD(0x27, 16, 2);
+const int a = 4;
 
-// Определение пинов для шаговых моторов
-#define MOTOR1_IN1 10
-#define MOTOR1_IN2 11
-#define MOTOR1_IN3 12
-#define MOTOR1_IN4 13
+// Определение пинов для VEX моторов
+#define VEX_MOTOR1_PWM 10
+#define VEX_MOTOR2_PWM 11
 
-#define MOTOR2_IN1 A0
-#define MOTOR2_IN2 A1
-#define MOTOR2_IN3 A2
-#define MOTOR2_IN4 A3
+Servo vexMotor1;
+Servo vexMotor2;
 
-int dl = 5; // время задержки между импульсами
-
-int Ver[a][a]; // Двумерный массив для хранения случайных чисел
-int NeVer[a]; // Массив для хранения введенных пользователем чисел
-int i = 0; // Счетчик для массива NeVer
-int n = 0; // Счетчик для проверки совпадений
-int wa = 0; // Переменная для подсчета неверных попыток
-long randNumber;
+int Ver[a][a];
+int NeVer[a];
+int i = 0;
+int n = 0;
+int wa = 0; // Счётчик неверных попыток
+unsigned long blockTime = 300000; // Начальное время блокировки (5 минут)
+const unsigned long baseBlockTime = 300000; // Базовая блокировка 5 минут
+const unsigned long additionalBlockTime = 300000; // Дополнительные 5 минут
 
 // Настройка клавиатуры
-const byte ROWS = 4; // Количество строк на клавиатуре
-const byte COLS = 4; // Количество столбцов на клавиатуре
+const byte ROWS = 4;
+const byte COLS = 4;
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'}, 
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  {'1','2','3','A'}, 
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {2, 3, 4, 5}; // Пины для подключения строк клавиатуры
-byte colPins[COLS] = {6, 7, 8, 9}; // Пины для подключения столбцов клавиатуры
-Keypad Klava = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); // Инициализация клавиатуры
+byte rowPins[ROWS] = {2, 3, 4, 5};
+byte colPins[COLS] = {6, 7, 8, 9};
+Keypad Klava = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 void vvediteParol() {
   LCD.clear();
@@ -54,15 +51,11 @@ void sbrosNeVer() {
 }
 
 void generiruyKody() {
-  // Заполнение массива Ver случайными числами
   for (int r = 0; r < a; r++) {
     for (int i = 0; i < a; i++) {
-      randNumber = random(0, 10);
-      Ver[r][i] = randNumber; // Заполнение массива случайными числами от 0 до 9
+      Ver[r][i] = random(0, 10);
     }
   }
-
-  // Вывод содержимого массива Ver в монитор порта для отладки
   for (int t = 0; t < a; t++) {
     Serial.print(t + 1);
     Serial.print(" ячейка - ");
@@ -73,220 +66,149 @@ void generiruyKody() {
   }
 }
 
-void setup() {
-  Serial.begin(9600); // Запуск последовательного соединения для отладки
-
-  // Настройка пинов для шаговых моторов
-  pinMode(MOTOR1_IN1, OUTPUT);
-  pinMode(MOTOR1_IN2, OUTPUT);
-  pinMode(MOTOR1_IN3, OUTPUT);
-  pinMode(MOTOR1_IN4, OUTPUT);
+void setVexMotorSpeed(Servo &motor, int speed) {
+  speed = constrain(speed, -100, 100);
+  int pulseWidth;
   
-  pinMode(MOTOR2_IN1, OUTPUT);
-  pinMode(MOTOR2_IN2, OUTPUT);
-  pinMode(MOTOR2_IN3, OUTPUT);
-  pinMode(MOTOR2_IN4, OUTPUT);
+  if (speed == 0) {
+    pulseWidth = 1500;
+  } else if (speed > 0) {
+    pulseWidth = map(speed, 0, 100, 1500, 2000);
+  } else {
+    pulseWidth = map(speed, -100, 0, 1000, 1500);
+  }
+  
+  motor.writeMicroseconds(pulseWidth);
+}
 
-  // Инициализация генератора случайных чисел
+void rotateMotor1Forward() { setVexMotorSpeed(vexMotor1, 100); }
+void rotateMotor1Backward() { setVexMotorSpeed(vexMotor1, -100); }
+void stopMotor1() { setVexMotorSpeed(vexMotor1, 0); }
+void rotateMotor2Forward() { setVexMotorSpeed(vexMotor2, 100); }
+void rotateMotor2Backward() { setVexMotorSpeed(vexMotor2, -100); }
+void stopMotor2() { setVexMotorSpeed(vexMotor2, 0); }
+
+void setup() {
+  Serial.begin(9600);
+  vexMotor1.attach(VEX_MOTOR1_PWM);
+  vexMotor2.attach(VEX_MOTOR2_PWM);
+  stopMotor1();
+  stopMotor2();
   randomSeed(analogRead(0));
-  generiruyKody(); // Генерация кодов только один раз при старте
-
-  LCD.init(); // Инициализация LCD дисплея
-  LCD.backlight(); // Включение подсветки дисплея
+  generiruyKody();
+  LCD.init();
+  LCD.backlight();
   LCD.clear();
   vvediteParol();
 }
 
-// Функция для вращения шагового мотора 1 (90 градусов по часовой стрелке)
-void rotateMotor1CW() {
-  for(int j = 0; j < 128; j++) { // 128 шагов примерно соответствуют 90 градусам
-    digitalWrite(MOTOR1_IN1, HIGH); 
-    digitalWrite(MOTOR1_IN2, LOW); 
-    digitalWrite(MOTOR1_IN3, LOW); 
-    digitalWrite(MOTOR1_IN4, HIGH);
-    delay(dl);
-
-    digitalWrite(MOTOR1_IN1, HIGH); 
-    digitalWrite(MOTOR1_IN2, HIGH); 
-    digitalWrite(MOTOR1_IN3, LOW); 
-    digitalWrite(MOTOR1_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR1_IN1, LOW); 
-    digitalWrite(MOTOR1_IN2, HIGH); 
-    digitalWrite(MOTOR1_IN3, HIGH); 
-    digitalWrite(MOTOR1_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR1_IN1, LOW); 
-    digitalWrite(MOTOR1_IN2, LOW); 
-    digitalWrite(MOTOR1_IN3, HIGH); 
-    digitalWrite(MOTOR1_IN4, HIGH);
-    delay(dl);
+void showBlockScreen(int minutes) {
+  LCD.clear();
+  LCD.setCursor(3, 0);
+  LCD.print("Blokirovka");
+  LCD.setCursor(2, 1);
+  LCD.print(minutes);
+  // Правильное склонение слова "минут"
+  if (minutes % 10 == 1 && minutes % 100 != 11) {
+    LCD.print(" minuta");
+  } else if ((minutes % 10 >= 2 && minutes % 10 <= 4) && !(minutes % 100 >= 12 && minutes % 100 <= 14)) {
+    LCD.print(" minuty");
+  } else {
+    LCD.print(" minut");
   }
 }
 
-// Функция для вращения шагового мотора 1 (90 градусов против часовой стрелки)
-void rotateMotor1CCW() {
-  for(int j = 0; j < 128; j++) { // 128 шагов примерно соответствуют 90 градусам
-    digitalWrite(MOTOR1_IN1, LOW); 
-    digitalWrite(MOTOR1_IN2, LOW); 
-    digitalWrite(MOTOR1_IN3, HIGH); 
-    digitalWrite(MOTOR1_IN4, HIGH);
-    delay(dl);
-
-    digitalWrite(MOTOR1_IN1, LOW); 
-    digitalWrite(MOTOR1_IN2, HIGH); 
-    digitalWrite(MOTOR1_IN3, HIGH); 
-    digitalWrite(MOTOR1_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR1_IN1, HIGH); 
-    digitalWrite(MOTOR1_IN2, HIGH); 
-    digitalWrite(MOTOR1_IN3, LOW); 
-    digitalWrite(MOTOR1_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR1_IN1, HIGH); 
-    digitalWrite(MOTOR1_IN2, LOW); 
-    digitalWrite(MOTOR1_IN3, LOW); 
-    digitalWrite(MOTOR1_IN4, HIGH);
-    delay(dl);
+void handleBlock() {
+  // Вычисляем количество полных 3-попыточных циклов
+  int blockCycles = (wa - 1) / 3;
+  blockTime = baseBlockTime + blockCycles * additionalBlockTime;
+  
+  int totalMinutes = blockTime / 60000;
+  showBlockScreen(totalMinutes);
+  
+  unsigned long startTime = millis();
+  while (millis() - startTime < blockTime) {
+    unsigned long remainingTime = blockTime - (millis() - startTime);
+    int remainingMinutes = remainingTime / 60000;
+    int remainingSeconds = (remainingTime % 60000) / 1000;
+    
+    LCD.setCursor(0, 0);
+    LCD.print("Blokirovka      ");
+    LCD.setCursor(0, 1);
+    LCD.print("Ostalos: ");
+    if (remainingMinutes < 10) LCD.print(" ");
+    LCD.print(remainingMinutes);
+    LCD.print("m ");
+    if (remainingSeconds < 10) LCD.print("0");
+    LCD.print(remainingSeconds);
+    LCD.print("s  ");
+    
+    delay(200);
   }
-}
-
-// Функции для управления вторым мотором (аналогично)
-void rotateMotor2CW() {
-  for(int j = 0; j < 128; j++) {
-    digitalWrite(MOTOR2_IN1, HIGH); 
-    digitalWrite(MOTOR2_IN2, LOW); 
-    digitalWrite(MOTOR2_IN3, LOW); 
-    digitalWrite(MOTOR2_IN4, HIGH);
-    delay(dl);
-
-    digitalWrite(MOTOR2_IN1, HIGH); 
-    digitalWrite(MOTOR2_IN2, HIGH); 
-    digitalWrite(MOTOR2_IN3, LOW); 
-    digitalWrite(MOTOR2_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR2_IN1, LOW); 
-    digitalWrite(MOTOR2_IN2, HIGH); 
-    digitalWrite(MOTOR2_IN3, HIGH); 
-    digitalWrite(MOTOR2_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR2_IN1, LOW); 
-    digitalWrite(MOTOR2_IN2, LOW); 
-    digitalWrite(MOTOR2_IN3, HIGH); 
-    digitalWrite(MOTOR2_IN4, HIGH);
-    delay(dl);
-  }
-}
-
-void rotateMotor2CCW() {
-  for(int j = 0; j < 128; j++) {
-    digitalWrite(MOTOR2_IN1, LOW); 
-    digitalWrite(MOTOR2_IN2, LOW); 
-    digitalWrite(MOTOR2_IN3, HIGH); 
-    digitalWrite(MOTOR2_IN4, HIGH);
-    delay(dl);
-
-    digitalWrite(MOTOR2_IN1, LOW); 
-    digitalWrite(MOTOR2_IN2, HIGH); 
-    digitalWrite(MOTOR2_IN3, HIGH); 
-    digitalWrite(MOTOR2_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR2_IN1, HIGH); 
-    digitalWrite(MOTOR2_IN2, HIGH); 
-    digitalWrite(MOTOR2_IN3, LOW); 
-    digitalWrite(MOTOR2_IN4, LOW);
-    delay(dl);
-
-    digitalWrite(MOTOR2_IN1, HIGH); 
-    digitalWrite(MOTOR2_IN2, LOW); 
-    digitalWrite(MOTOR2_IN3, LOW); 
-    digitalWrite(MOTOR2_IN4, HIGH);
-    delay(dl);
-  }
+  
+  LCD.clear();
+  vvediteParol();
 }
 
 void loop() {
-  char simvol = Klava.getKey(); // Считывание нажатой клавиши
+  char simvol = Klava.getKey();
 
-  if (simvol) { // Если клавиша нажата
-    if (simvol == '*' && i > 0) { // Если нажата '*' и есть введенные символы
-      i--; // Уменьшаем счетчик
-      NeVer[i] = 0; // Сбрасываем значение
+  if (simvol) {
+    if (simvol == '*' && i > 0) {
+      i--;
+      NeVer[i] = 0;
       LCD.setCursor(0, 0);
       LCD.print("Vvedite kod:");
       LCD.setCursor(0, 1);
       for (int j = 0; j < a; j++) {
-        if (j < i) {
-          LCD.print(NeVer[j]); // Выводим введенные символы
-        } else {
-          LCD.print(" "); // Очищаем оставшиеся позиции
-        }
+        if (j < i) LCD.print(NeVer[j]);
+        else LCD.print(" ");
       }
     }
-    else if (i < a && isdigit(simvol)) { // Если введена цифра и есть место в массиве
-      NeVer[i] = simvol - '0'; // Преобразование символа в число
+    else if (i < a && isdigit(simvol)) {
+      NeVer[i] = simvol - '0';
       LCD.setCursor(0, 0);
       LCD.print("Vvedite kod:");
       LCD.setCursor(i, 1);
-      LCD.print(NeVer[i]); // Отображение введенной цифры
-      i++; // Увеличение счетчика введенных символов
+      LCD.print(NeVer[i]);
+      i++;
     }
 
-    if (i == a) { // Если введено 4 символа
-      // Проверка совпадения введенного кода с одним из кодов в массиве Ver
+    if (i == a) {
       for (int q = 0; q < a; q++) {
-        n = 0; // Сброс счетчика совпадений перед каждой проверкой
+        n = 0;
         for (int e = 0; e < a; e++) {
-          if (Ver[q][e] == NeVer[e]) {
-            n++; // Увеличение счетчика совпадений
-          }
+          if (Ver[q][e] == NeVer[e]) n++;
         }
 
-        if (n == a) { // Если все символы совпали
+        if (n == a) {
           LCD.clear();
           LCD.setCursor(3, 0);
           LCD.print("nazhmite #");
           LCD.setCursor(1, 1);
           LCD.print("dlya zakritiya");
           
-          // Выполняем соответствующее действие для каждого кода
           if (q == 0) {
-            rotateMotor1CW(); // Вращение первого мотора по часовой стрелке
+            rotateMotor1Forward();
+            delay(2000);
+            stopMotor1();
           } else if (q == 1) {
-            rotateMotor1CCW(); // Вращение первого мотора против часовой стрелки
+            rotateMotor1Backward();
+            delay(2000);
+            stopMotor1();
           } else if (q == 2) {
-            rotateMotor2CW(); // Вращение второго мотора по часовой стрелке
+            rotateMotor2Forward();
+            delay(2000);
+            stopMotor2();
           } else if (q == 3) {
-            rotateMotor2CCW(); // Вращение второго мотора против часовой стрелки
+            rotateMotor2Backward();
+            delay(2000);
+            stopMotor2();
           }
-          
-          // Ожидание нажатия клавиши '#' для возврата
-          while (true) {
-            char simvol = Klava.getKey();
-            if (simvol == '#') {
-              // Возвращаем мотор в исходное положение
-              if (q == 0) {
-                rotateMotor1CCW(); // Возврат первого мотора
-              } else if (q == 1) {
-                rotateMotor1CW(); // Возврат первого мотора
-              } else if (q == 2) {
-                rotateMotor2CCW(); // Возврат второго мотора
-              } else if (q == 3) {
-                rotateMotor2CW(); // Возврат второго мотора
-              }
-              break;
-            }
-          }
-          
+                    
           LCD.clear();
-          wa = 0; // Сброс счетчика неверных попыток при успешном вводе
+          wa = 0;
+          blockTime = baseBlockTime;
           vvediteParol();
           i = 0;
           sbrosNeVer();
@@ -294,31 +216,24 @@ void loop() {
         }
       }
 
-      if (n != a) { // Если код не совпал
-        wa++;
-        if (wa >= 3) { // Блокировка после 3 неудачных попыток
-          LCD.clear();
-          LCD.setCursor(3, 0);
-          LCD.print("Blokirovka");
-          LCD.setCursor(4, 1);
-          LCD.print("3 minuty");
-          delay(180000); // Блокировка на 3 минуты (180000 миллисекунд)
-          wa = 0; // Сброс счетчика неверных попыток
-        } else {
-          LCD.clear();
-          LCD.setCursor(1, 0);
-          LCD.print("Podozhdite 15s");
-          for (int t = 15; t > 0; t--) { // Обратный отсчет от 15 до 1
-            LCD.setCursor(2, 1);
-            LCD.print("Ostalos: ");
-            LCD.print(t);
-            LCD.print("s  ");
-            delay(1000); // Задержка на 1 секунду
-          }
+      // Неверный пароль
+      wa++;
+      
+      if (wa >= 3) {
+        handleBlock();
+      } else {
+        LCD.clear();
+        LCD.setCursor(1, 0);
+        LCD.print("Podozhdite 15s");
+        for (int t = 15; t > 0; t--) {
+          LCD.setCursor(2, 1);
+          LCD.print("Ostalos: ");
+          LCD.print(t);
+          LCD.print("s  ");
+          delay(1000);
         }
       }
 
-      // Сброс и подготовка к новому вводу
       i = 0;
       sbrosNeVer();
       vvediteParol();
